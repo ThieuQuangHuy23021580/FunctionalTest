f451d76f9607c921f8ee377ee76fef852ca2847a
ThieuQuangHuy23021580 <160751958+ThieuQuangHuy23021580@users.noreply.github.com>
2026-04-11 15:19:33 +0700

    DataFlowTesting

--- src/test/java/Testing/DataFlowTesting.java
+++ src/test/java/Testing/DataFlowTesting.java
@@ -1,5 +1,27 @@
 package Testing;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DataFlowTesting {
-    
+
+    @Test
+    @DisplayName("TC1 [path: 1-2(F)-5(F)-6(REGULAR)-7(F)-13(F)-19(T)-22] input=(7, REGULAR) EO=10")
+    void dataFlowTest1() {
+        assertEquals(10, CoffeeDiscount.getDiscount(7, "REGULAR"));
+    }
+
+    @Test
+    @DisplayName("TC2 [path: 1-2(F)-5(F)-6(MEMBER)-8(F)-14(F)-20(T)-23] input=(7, MEMBER) EO=15")
+    void dataFlowTest2() {
+        assertEquals(15, CoffeeDiscount.getDiscount(7, "MEMBER"));
+    }
+
+    @Test
+    @DisplayName("TC3 [path: 1-2(F)-5(F)-6(VIP)-9(F)-15(F)-21(T)-24] input=(7, VIP) EO=20")
+    void dataFlowTest3() {
+        assertEquals(20, CoffeeDiscount.getDiscount(7, "VIP"));
+    }
 }
